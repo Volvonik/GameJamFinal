111b5411f8acb45b792a759719a1fd3b4b7cb2e7
Ran2009 <ryanuka09@gmail.com>
2023-07-08 11:10:21 +0300

    made bullet destroy on collision

--- Zombies vs Plants - The War/Assets/scripts/bullet_script.cs
+++ Zombies vs Plants - The War/Assets/scripts/bullet_script.cs
@@ -7,7 +7,7 @@ public class bullet_script : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody2D rb;
     public float bullet_speed = 1f;
-    public GameObject self;
+    public GameObject bullet;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,10 +19,11 @@ public class bullet_script : MonoBehaviour
     {
         rb.velocity = new Vector2(bullet_speed * Time.fixedDeltaTime, 0);
     }
-    private void OnTriggerEnter(Collider other){
+    private void OnTriggerEnter2D(Collider2D other){
         if (other.tag == "Player"){
-            Destroy(self);
+            Destroy(gameObject);
         }
+        
         
     }
 
