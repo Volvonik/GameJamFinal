bb777c025579067fe48d92e39048ab1edb298cc8
Ran2009 <ryanuka09@gmail.com>
2023-07-08 19:17:14 +0300

    zombie spawner

--- Zombies vs Plants - The War/Assets/scripts/zombieSpawnerScript.cs
+++ Zombies vs Plants - The War/Assets/scripts/zombieSpawnerScript.cs
@@ -16,8 +16,36 @@ public class zombieSpawnerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)){
           Vector3 MousePosition = Input.mousePosition;
-          Instantiate(zombie,new Vector3(8.25f,MousePosition.y,0), transform.rotation);
-          Debug.Log("button pressed");
+          MousePosition.z = Camera.main.nearClipPlane +1;
+          Vector3 worldPosition = Camera.main.ScreenToWorldPoint(MousePosition);
+          Instantiate(zombie,new Vector3(4.8f,Block(worldPosition.y),0), transform.rotation);
+          //Debug.Log("button pressed");
         }
+    }
+    float Block(float y){
+        if (y <= 2f && y > 1f){
+            return 1.25f;
+        }
+        else if (y <=1f && y>0f)
+        {
+            return 0.25f;
+        }
+        else if (y <= 0f && y>-1f)
+        {
+            return -0.725f;
+        }
+        else if (y <= -1f && y>-2f )
+        {
+            return -1.725f;
+        }
+        else if (y<= -2f && y>-3f)
+        {
+            return -2.725f;
+        }
+        else{
+            return -0.725f;
+        }
+        
+            
     }
 }
