2a674744b7cc28c2a23ff27ea0f0dd8be6d9c00d
Ran2009 <ryanuka09@gmail.com>
2023-07-08 16:36:10 +0300

    particel system

--- Zombies vs Plants - The War/Assets/scripts/PotatoScript.cs
+++ Zombies vs Plants - The War/Assets/scripts/PotatoScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PotatoScript : MonoBehaviour
 {
     public float potato_health = 4f;
+    private ParticleSystem particelSystem;
+    float ParticelTimer = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        particelSystem = GetComponent<ParticleSystem>();
+        particelSystem.Stop();
+
     }
 
     // Update is called once per frame
@@ -17,11 +21,23 @@ public class PotatoScript : MonoBehaviour
         if (potato_health <= 0f){
             Destroy(gameObject);
         }
+        if (ParticelTimer >= 0.5f){
+            particelSystem.Stop();
+        }
+        ParticelTimer += Time.deltaTime;
     }
     private void OnCollisionStay2D(Collision2D collider){
         if (collider.gameObject.tag == "Player"){
             potato_health -= Time.deltaTime;
             //Debug.Log(health);
+            //var particelSystemMain = particelSystem.main;
+            //particelSystemMain.startLifeTime = 1f;
+            particelSystem.Play();
+            ParticelTimer = 0f;
+            
+
+
+
         }
     }
 
