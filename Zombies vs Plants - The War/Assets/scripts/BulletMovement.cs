62b85dc8cbabfbb14e524cedc21aad0e9f023126
Volvonik <volvo0unity@gmail.com>
2023-07-08 15:51:28 +0300

    Only one Scene

--- Zombies vs Plants - The War/Assets/scripts/BulletMovement.cs
+++ Zombies vs Plants - The War/Assets/scripts/BulletMovement.cs
@@ -6,7 +6,11 @@ public class BulletMovement : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    Vector3 spawnerPosition;
+
     public float bullet_speed = 3f;
+
+    public float bulletRange = 4f;
 
     public bool bulletHit = false;
 
@@ -14,19 +18,35 @@ public class BulletMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
 
+        spawnerPosition = transform.position;
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(bullet_speed * Time.fixedDeltaTime, 0);
+
+        Vector3 bulletPosition = transform.position;
+
+        if (bulletPosition.x - spawnerPosition.x >= bulletRange)
+        {
+            Debug.Log("No Targets");
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (bulletHit == true)
+        {
+            bulletHit = false;
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other){
         if (other.tag == "Player")
         {
             bulletHit = true;
-            Destroy(gameObject);
-            bulletHit = false;
         }
         
         
--- Zombies vs Plants - The War/Assets/scripts/BulletSpawnerScript.cs
+++ Zombies vs Plants - The War/Assets/scripts/BulletSpawnerScript.cs
@@ -9,12 +9,6 @@ public class BulletSpawnerScript : MonoBehaviour
     public float spawnRate = 1f;
     private float spawnRateTimer = 0f; 
 
-    public float bulletRange = 2f;
-
-    public LayerMask layersToHit;
-
-    bool bulletHasTarget = false;
-
     void Start()
     {
         spawnRateTimer = 0f;
@@ -27,34 +21,8 @@ public class BulletSpawnerScript : MonoBehaviour
         if (spawnRateTimer >= spawnRate)
         {
             Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
+
             spawnRateTimer = 0f;
-        }
-
-        if (bulletHasTarget == false)
-        {
-            Debug.Log("Jeez");
-        }
-        else if (bulletHasTarget == true)
-        {
-            Debug.Log("Yay!");
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, bulletRange * 2);
-        Debug.DrawRay(transform.position, Vector2.right * hit.distance, Color.red);
-
-        if (hit.collider != null)
-        {
-            bulletHasTarget = false;
-            Debug.Log(bulletHasTarget);
-        }
-
-        else
-        {
-            bulletHasTarget = true;
-            Debug.Log(bulletHasTarget);
         }
     }
 }
