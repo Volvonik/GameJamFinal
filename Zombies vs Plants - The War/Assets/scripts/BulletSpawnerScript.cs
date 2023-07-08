81cdd9ac767d9307aedd071e8b84a431749430f1
Volvonik <volvo0unity@gmail.com>
2023-07-08 15:06:34 +0300

    Im trying a RayCast for the bullet's range but im going to eat

--- Zombies vs Plants - The War/Assets/scripts/BulletSpawnerScript.cs
+++ Zombies vs Plants - The War/Assets/scripts/BulletSpawnerScript.cs
@@ -4,22 +4,57 @@ using UnityEngine;
 
 public class BulletSpawnerScript : MonoBehaviour
 {
-    
     public GameObject bullet;
-    public float spawn_rate = 1f;
-    private float timer = 0f; 
+
+    public float spawnRate = 1f;
+    private float spawnRateTimer = 0f; 
+
+    public float bulletRange = 2f;
+
+    public LayerMask layersToHit;
+
+    bool bulletHasTarget = false;
 
     void Start()
     {
-        timer = 0f;
+        spawnRateTimer = 0f;
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= spawn_rate){
+        spawnRateTimer += Time.deltaTime;
+
+        if (spawnRateTimer >= spawnRate)
+        {
             Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
-            timer = 0f;
+            spawnRateTimer = 0f;
+        }
+
+        if (bulletHasTarget == false)
+        {
+            Debug.Log("Jeez");
+        }
+        else if (bulletHasTarget == true)
+        {
+            Debug.Log("Yay!");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, bulletRange * 2);
+        Debug.DrawRay(transform.position, Vector2.right * hit.distance, Color.red);
+
+        if (hit.collider != null)
+        {
+            bulletHasTarget = false;
+            Debug.Log(bulletHasTarget);
+        }
+
+        else
+        {
+            bulletHasTarget = true;
+            Debug.Log(bulletHasTarget);
         }
     }
 }
