28692d5c948cafc484176ea56e142d0b8a3fd510
Ran2009 <ryanuka09@gmail.com>
2023-07-09 19:15:19 +0300

    Revert "a"
    This reverts commit dd9419ad4cd74c2d9f583461a61ebb449564d3cd.

--- Zombies vs Plants - The War/Assets/Scripts/BluePlantScripts/BluePlantScript.cs
+++ Zombies vs Plants - The War/Assets/Scripts/BluePlantScripts/BluePlantScript.cs
@@ -7,32 +7,22 @@ public class BluePlantScript : MonoBehaviour
     public GameObject myPlant;
     public MoneyScript ms;
     public float BluePlantValue = 100f;
-    public ParticleSystem eatingEffect;
-    public ParticleSystem dieEffecct;
+
     public float blueHealth = 3f;
-    public MoneyScript money_script;
- void Update()
+
+    void Update()
     {
         if (blueHealth <= 0f)
         {
-            Die();
-            Destroy(gameObject);
-            money_script.AddMoney(BluePlantValue);
+            Destroy(myPlant);
+            ms.AddMoney(BluePlantValue);
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision){
-        
-        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Zombie")
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Zombie")
         {
             blueHealth -= Time.deltaTime;
-            eatingEffect.Play();
         }
-
-    }
-    void Die()
-    {
-        eatingEffect.Stop();
-        dieEffecct.Play();
-        Destroy(gameObject);
     }
 }
